4c4c0a351f293573e4593be27cb4328203150be8
jamesdavy21 <jamesdavy21@hotmail.co.uk>
2019-12-15 16:54:15 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# KeyStage3MathsTutor
---
+++ app/src/main/java/com/example/up804392/keystage3mathstutor/ui/about/AboutFragment.java
@@ -0,0 +1,23 @@
+package com.example.up804392.keystage3mathstutor.ui.about;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.up804392.keystage3mathstutor.R;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class AboutFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+
+        return view;
+    }
+}
