6fedac0e99a0e702858daa1d38bf40dc63ad9bc6
jamesdavy21 <jamesdavy21@hotmail.co.uk>
2019-12-16 01:05:48 +0000

    update to add bottom navigation bar

--- app/src/main/java/com/example/up804392/keystage3mathstutor/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/up804392/keystage3mathstutor/ui/home/HomeFragment.java
@@ -2,17 +2,15 @@ package com.example.up804392.keystage3mathstutor.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.up804392.keystage3mathstutor.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
@@ -22,8 +20,32 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        TextView textView = view.findViewById(R.id.text_home);
-        textView.setText("home page");
+        BottomNavigationView navView = view.findViewById(R.id.nav_bottom_view);
+        navView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        navView.setSelectedItemId(R.id.navigation_quiz);
+        getChildFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeQuizFragment()).commit();
         return view;
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment = null;
+
+            switch (menuItem.getItemId()) {
+                default:
+                    fragment = new HomeQuizFragment();
+                    break;
+                case R.id.navigation_guide:
+                    fragment = new HomeGuideFragment();
+                    break;
+                case R.id.navigation_game:
+                    fragment = new HomeGuideFragment();
+                    break;
+            }
+            getChildFragmentManager().popBackStack();
+            getChildFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+    };
 }
---
+++ app/src/main/java/com/example/up804392/keystage3mathstutor/ui/home/HomeGuideFragment.java
@@ -0,0 +1,21 @@
+package com.example.up804392.keystage3mathstutor.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.up804392.keystage3mathstutor.R;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class HomeGuideFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_guide, container, false);
+
+        return view;
+    }
+}
